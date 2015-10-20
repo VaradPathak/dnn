476c6673277ef1848904c3ea1ff479920be97f01
Varad Pathak <pathak.varad@gmail.com>
2015-10-20 16:04:49 -0700

    Init

---
+++ Perceptron.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 20, 2015
+
+@author: varadpathak
+'''
+
+class Perceptron(object):
+    '''
+    Basic Perceptron in Neural Network
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
